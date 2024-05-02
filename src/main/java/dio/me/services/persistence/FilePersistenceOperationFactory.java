86f38c1bff92e1a639cb9fa597b9a3e141ee1784
LuigiPereira1709 <luigipereira1001@gmail.com>
2024-05-02 19:16:00 -0300

    feat: factory for persistence operations

--- src/main/java/dio/me/services/persistence/FilePersistenceOperationFactory.java
+++ src/main/java/dio/me/services/persistence/FilePersistenceOperationFactory.java
@@ -1,2 +1,21 @@
-package main.java.dio.me.services.persistence;public class FilePersistenceOperationFactory {
+package main.java.dio.me.services.persistence;
+
+public final class FilePersistenceOperationFactory {
+
+    /**
+     * Creates a persistence operation object based on the specified operation type.
+     *
+     * @param operation The type of persistence operation.
+     * @return An instance of PersistencePerform representing the specified operation.
+     * @throws IllegalArgumentException if the specified operation is unsupported.
+     */
+    @SuppressWarnings("rawtypes")
+    public static PersistencePerform createOperation(PersistenceOperation operation) {
+        return switch (operation) {
+            case SAVE -> new SavePerform();
+            case UPDATE -> new UpdatePerform();
+            case LOAD -> new LoadPerform();
+            case DELETE -> new DeletePerform();
+        };
+    }
 }
