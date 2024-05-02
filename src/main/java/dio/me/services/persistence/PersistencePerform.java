c3fba30c870a5ff48dca58a80d2ecbb43f41effc
LuigiPereira1709 <luigipereira1001@gmail.com>
2024-05-02 19:14:46 -0300

    feat: interface for Persistence operations

--- src/main/java/dio/me/services/persistence/PersistencePerform.java
+++ src/main/java/dio/me/services/persistence/PersistencePerform.java
@@ -1,14 +1,23 @@
 package main.java.dio.me.services.persistence;
 
-import java.io.File;
 import java.util.Map;
-import java.util.Objects;
 
+/**
+ * An interface representing a persistence operation.
+ *
+ * @param <T> The type of result produced by executing the operation.
+ */
 public interface PersistencePerform<T> {
     T execute(String accountData);
 }
 
 class SavePerform implements PersistencePerform<Void> {
+    /**
+     * Executes the operation to save account data.
+     *
+     * @param accountData The account data to be saved.
+     * @return null, as the operation does not produce a return value.
+     */
     @Override
     public Void execute(String accountData) {
         FilePersistenceService.saveData(accountData);
@@ -17,6 +26,12 @@ class SavePerform implements PersistencePerform<Void> {
 }
 
 class UpdatePerform implements PersistencePerform<Void> {
+    /**
+     * Executes the operation to update account data.
+     *
+     * @param accountData The account data to be updated.
+     * @return null, as the operation does not produce a return value.
+     */
     @Override
     public Void execute(String accountData) {
         FilePersistenceService.updateData(accountData);
@@ -24,19 +39,31 @@ class UpdatePerform implements PersistencePerform<Void> {
     }
 }
 
-class LoadPerform implements PersistencePerform<Map<String, Object>> {
+@SuppressWarnings("rawtypes")
+class LoadPerform implements PersistencePerform<Map> {
+    /**
+     * Executes the operation to load account data.
+     *
+     * @param accountData Unused parameter.
+     * @return A map containing the loaded account data.
+     */
     @Override
-    public Map<String, Object> execute(String accountData) {
-        Map<String, Object> mapData = FilePersistenceService.loadData();
-        return mapData;
+    public Map execute(String accountData) {
+        return FilePersistenceService.loadData();
     }
 }
 
+
 class DeletePerform implements PersistencePerform<Void> {
+    /**
+     * Executes the operation to delete account data.
+     *
+     * @param accountData The account data to be deleted.
+     * @return null, as the operation does not produce a return value.
+     */
     @Override
     public Void execute(String accountData) {
         FilePersistenceService.deleteData(accountData);
         return null;
     }
 }
-
